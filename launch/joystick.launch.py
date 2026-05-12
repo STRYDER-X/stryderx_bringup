2d4051575350d9983c6ca428ac6d45e4607da85b
Julian A Rendon <julianrendon514@gmail.com>
2026-05-11 20:36:15 -0400

    Add joystick params.

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -2,6 +2,9 @@
 
 """Launch file for the joystick node."""
 
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
@@ -9,10 +12,30 @@ from launch import LaunchDescription
 
 def generate_launch_description() -> LaunchDescription:
     """Generate a launch description for the joystick node."""
-    joy_node = Node(
+    package_share_dir = get_package_share_directory("stryderx_bringup")
+
+    joystick_config_file = os.path.join(
+        package_share_dir,
+        "config",
+        "joystick_params.yaml",
+    )
+
+    joystick_node = Node(
         package="joy",
         executable="joy_node",
         output="screen",
     )
 
-    return LaunchDescription([joy_node])
+    joystick_teleop_node = Node(
+        package="stryderx_hardware",
+        executable="joystick_teleop",
+        output="screen",
+        parameters=[joystick_config_file],
+    )
+
+    return LaunchDescription(
+        [
+            joystick_node,
+            joystick_teleop_node,
+        ]
+    )
